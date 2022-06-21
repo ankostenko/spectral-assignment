e65449fa4b3496ffc3d8280a20959598bb6daa7b
ankostenko <andreykostenko99@gmail.com>
2022-06-21 15:40:03 +0400

    Fixes test for default request, add test for request with different page size

--- client/spectral/tests/page/test_request.py
+++ client/spectral/tests/page/test_request.py
@@ -6,13 +6,26 @@ from spectral.gen_grpc.meterusage_pb2 import UsageRequest
 
 
 class TestRequest(object):
-    def test_all_data_request(self, client):
+    def test_default_data_request(self, client):
         """
-            Tests that all requested retrieved from file
+            Tests that default amount of data retrieved from server
         """
         # Connect to grpc server and retrieve usage data
         with grpc.insecure_channel('grpc_server:50051') as channel:
             stub = MeterUsageStub(channel)
             response = stub.GetUsageData(UsageRequest(
                                                 usage_stat_name='meterusage'))
-            assert len(list(response.datetime)) == 2976
+            assert len(list(response.datetime)) == 20
+
+
+    def test_fifty_data_request(self, client):
+        """
+            Tests that default amount of data retrieved from server
+        """
+        # Connect to grpc server and retrieve usage data
+        with grpc.insecure_channel('grpc_server:50051') as channel:
+            stub = MeterUsageStub(channel)
+            response = stub.GetUsageData(UsageRequest(
+                                                usage_stat_name='meterusage',
+                                                page_size=50))
+            assert len(list(response.datetime)) == 50
